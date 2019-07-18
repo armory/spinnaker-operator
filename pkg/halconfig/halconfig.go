dbf511f39e80d93853e8f040468b0de8be77c900
Nicolas Cohen <nicolas.cohen@armory.io>
2019-07-17 18:19:59 -0700

    chore(halconfig): minimal halconfig parser

---
+++ pkg/halconfig/halconfig.go
@@ -0,0 +1,25 @@
+package halconfig
+
+import "gopkg.in/yaml.v2"
+
+type SpinnakerCompleteConfig struct {
+	Files     map[string]string `json:"files",omitempty`
+	BinaryFiles map[string][]byte `json:"binary",omitempty`
+	Profiles map[string]string  `json:"profiles",omitempty`
+	HalConfig *HalConfig         `json:"halConfig",omitempty`
+}
+
+type HalConfig struct {
+	Version               string                `json:"version",omitempty`
+	DeploymentEnvironment DeploymentEnvironment `json:"deploymentEnvironment",omitempty`
+}
+
+type DeploymentEnvironment struct {
+	Type string `json:"type",omitempty`
+}
+
+func ParseHalConfig(data []byte) (HalConfig, error) {
+	hc := HalConfig{}
+	err := yaml.Unmarshal(data, &hc)
+	return hc, err
+}
