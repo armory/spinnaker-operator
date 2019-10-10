13ecf020917e4183730652f44d4208f14f90b283
Kevin Woo <kevinawoo@gmail.com>
2019-10-09 18:26:32 -0700

    tests(halyard): add test for halconfig parsing (#28)

--- go.mod
+++ go.mod
@@ -8,7 +8,7 @@ require (
 	github.com/emicklei/go-restful v2.8.1+incompatible // indirect
 	github.com/go-logr/logr v0.1.0
 	github.com/go-logr/zapr v0.1.0 // indirect
-	github.com/go-openapi/spec v0.18.0 // indirect
+	github.com/go-openapi/spec v0.18.0
 	github.com/gogo/protobuf v1.3.0 // indirect
 	github.com/golang/groupcache v0.0.0-20190702054246-869f871628b6 // indirect
 	github.com/google/btree v1.0.0 // indirect
--- pkg/halyard/halyard_test.go
+++ pkg/halyard/halyard_test.go
@@ -2,38 +2,158 @@ package halyard
 
 import (
 	"context"
+	"io/ioutil"
+	"net/http"
+	"reflect"
 	"strings"
 	"testing"
 
-	"io/ioutil"
-
-	halconfig "github.com/armory/spinnaker-operator/pkg/halconfig"
+	"github.com/armory/spinnaker-operator/pkg/halconfig"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestRequest(t *testing.T) {
-	s := Service{url: "http://localhost:8064"}
-	hc := &halconfig.SpinnakerConfig{}
-	c := `
+func makeBasicSpinnakerConfig() *halconfig.SpinnakerConfig {
+	halconfigYML := `
 name: default
 version: 1.14.2
 deploymentEnvironment:
   size: SMALL
+  type: Distributed`
+
+	hc := &halconfig.SpinnakerConfig{}
+	_ = hc.ParseHalConfig([]byte(halconfigYML))
+	return hc
+}
+
+func TestService_newHalyardRequest(t *testing.T) {
+	type fields struct {
+		url string
+	}
+	type args struct {
+		ctx        context.Context
+		spinConfig *halconfig.SpinnakerConfig
+	}
+
+	tests := []struct {
+		name     string
+		fields   fields
+		args     args
+		expected func(t *testing.T, got *http.Request)
+		wantErr  bool
+	}{
+		{
+			name:   "simple config with nothing else",
+			fields: fields{url: "http://localhost:8086"},
+			args: args{
+				ctx:        context.TODO(),
+				spinConfig: makeBasicSpinnakerConfig(),
+			},
+
+			wantErr: false,
+			expected: func(t *testing.T, got *http.Request) {
+				_ = got.ParseMultipartForm(32 << 20)
+
+				assert.Equal(t, len(got.MultipartForm.File), 1)
+
+				if f, _, err := got.FormFile("config"); assert.Nil(t, err) {
+					if gotBody, err := ioutil.ReadAll(f); assert.Nil(t, err) {
+						expectedBody := []byte(`
+deploymentEnvironment:
+  size: SMALL
   type: Distributed
-`
-	err := hc.ParseHalConfig([]byte(c))
-	if assert.Nil(t, err) {
-		req, err := s.newHalyardRequest(context.TODO(), hc)
-		if assert.Nil(t, err) {
-			f, _, err := req.FormFile("config")
-			if assert.Nil(t, err) {
-				b, err := ioutil.ReadAll(f)
-				if assert.Nil(t, err) {
-					assert.True(t, strings.Contains(string(b), "deploymentEnvironment"))
-					assert.True(t, strings.Contains(string(b), "version"))
+name: default
+version: 1.14.2`)
+
+						if !reflect.DeepEqual(strings.TrimSpace(string(gotBody)), strings.TrimSpace(string(expectedBody))) {
+							t.Errorf("newHalyardRequest() got body:\n'%s' \n\nexpected body:\n'%s'", gotBody, expectedBody)
+						}
+					}
 				}
+			},
+		},
+		{
+			name:   "make sure deck profile returns correctly",
+			fields: fields{url: "http://localhost:8086"},
+			args: args{
+				ctx: context.TODO(),
+				spinConfig: (func() *halconfig.SpinnakerConfig {
+					hc := makeBasicSpinnakerConfig()
+					hc.Profiles = map[string]interface{}{}
+					hc.Profiles["deck"] = `windows.settings = 55;`
+					return hc
+				})(),
+			},
+
+			wantErr: false,
+			expected: func(t *testing.T, got *http.Request) {
+				_ = got.ParseMultipartForm(32 << 20)
+
+				assert.Equal(t, len(got.MultipartForm.File), 2)
+
+				if f, _, err := got.FormFile("profiles__settings-local.js"); assert.Nil(t, err) {
+					if gotBody, err := ioutil.ReadAll(f); assert.Nil(t, err) {
+						expectedBody := []byte(`windows.settings = 55;`)
+
+						if !reflect.DeepEqual(strings.TrimSpace(string(gotBody)), strings.TrimSpace(string(expectedBody))) {
+							t.Errorf("newHalyardRequest() got body:\n'%s' \n\nexpected body:\n'%s'", gotBody, expectedBody)
+						}
+					}
+				}
+			},
+		},
+
+		{
+			name:   "make sure clouddriver profile returns correctly",
+			fields: fields{url: "http://localhost:8086"},
+			args: args{
+				ctx: context.TODO(),
+				spinConfig: (func() *halconfig.SpinnakerConfig {
+					hc := makeBasicSpinnakerConfig()
+					hc.Profiles = map[string]interface{}{}
+					hc.Profiles["clouddriver"] = `
+hello:
+  world: 48`
+					return hc
+				})(),
+			},
+
+			wantErr: false,
+			expected: func(t *testing.T, got *http.Request) {
+				_ = got.ParseMultipartForm(32 << 20)
+
+				assert.Equal(t, len(got.MultipartForm.File), 2)
+
+				if f, _, err := got.FormFile("profiles__clouddriver-local.yml"); assert.Nil(t, err) {
+					if gotBody, err := ioutil.ReadAll(f); assert.Nil(t, err) {
+
+						// note: halyard requires this field to be yaml
+						expectedBody := []byte(`|2-
+
+  hello:
+    world: 48`)
+
+						if !reflect.DeepEqual(strings.TrimSpace(string(gotBody)), strings.TrimSpace(string(expectedBody))) {
+							t.Errorf("newHalyardRequest() got body:\n'%s' \n\nexpected body:\n'%s'", gotBody, expectedBody)
+						}
+					}
+				}
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &Service{
+				url: tt.fields.url,
 			}
-		}
+			got, err := s.newHalyardRequest(tt.args.ctx, tt.args.spinConfig)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("newHalyardRequest() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			tt.expected(t, got)
+		})
 	}
 }
