9f85fb471ebaca63f1e82b5401385732960a8bb0
Ezequiel Rosas <ezequiel.rosas@digitalonus.com>
2020-04-27 17:17:53 -0500

    feat(defaults) Add test alreadyset

--- pkg/deploy/spindeploy/transformer/defaults_test.go
+++ pkg/deploy/spindeploy/transformer/defaults_test.go
@@ -32,3 +32,31 @@ spec:
 	archaius := archaius_.(interfaces.FreeForm)
 	assert.Equal(t, false, archaius["enabled"])
 }
+
+func TestConfig_SetArchaiusDefaults_alreadyTrue(t *testing.T) {
+	s := `
+apiVersion: spinnaker.io/v1alpha2
+kind: SpinnakerService
+metadata:
+  name: spinnaker
+spec:
+  spinnakerConfig:
+    profiles:
+      gate:
+        archaius:
+          enabled: true
+`
+	tr, spinsvc := th.setupTransformerFromSpinText(&defaultsTransformerGenerator{}, s, t)
+	err := tr.TransformConfig(context.TODO())
+	assert.Nil(t, err)
+
+	config := spinsvc.GetSpinnakerConfig()
+	gate := config.Profiles["gate"]
+	assert.NotNil(t, gate)
+
+	archaius_ := gate["archaius"]
+	assert.IsType(t, map[string]interface{}{}, archaius_)
+
+	archaius := archaius_.(map[string]interface{})
+	assert.Equal(t, true, archaius["enabled"])
+}
