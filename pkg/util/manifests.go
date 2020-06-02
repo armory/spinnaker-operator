f260c34ed624e77ec33e2faf6093b400dd589ac2
German Muzquiz <german.muzquiz@armory.io>
2020-06-02 14:51:26 -0500

    feat(util): Util function to add volumes to deployments

--- pkg/util/manifests.go
+++ pkg/util/manifests.go
@@ -35,3 +35,49 @@ func AddEnvVarToDeployment(d *appsv1.Deployment, e v1.EnvVar, appendIfExists boo
 	}
 	d.Spec.Template.Spec.Containers = ctrs
 }
+
+// AddVolumeMountAsSecret creates a new volume and volume mount for the given secret and adds it to all containers for which the filter function returns true
+func AddVolumeMountAsSecret(d *appsv1.Deployment, s *v1.Secret, mountPath string, filter func(c v1.Container) bool) {
+	addSecretVolumeIfNeeded(s.Name, d)
+	addSecretMountIfNeeded(s.Name, mountPath, d, filter)
+}
+
+func addSecretVolumeIfNeeded(secretName string, d *appsv1.Deployment) {
+	vols := make([]v1.Volume, 0)
+	for _, vi := range d.Spec.Template.Spec.Volumes {
+		vols = append(vols, vi)
+		if vi.Name == secretName {
+			return
+		}
+	}
+	vols = append(vols, v1.Volume{
+		Name: secretName,
+		VolumeSource: v1.VolumeSource{
+			Secret: &v1.SecretVolumeSource{
+				SecretName: secretName,
+			},
+		},
+	})
+	d.Spec.Template.Spec.Volumes = vols
+}
+
+func addSecretMountIfNeeded(volumeName, mountPath string, d *appsv1.Deployment, filter func(c v1.Container) bool) {
+	ctrs := make([]v1.Container, 0)
+	for _, c := range d.Spec.Template.Spec.Containers {
+		if !filter(c) {
+			ctrs = append(ctrs, c)
+			continue
+		}
+		added := false
+		for _, vm := range c.VolumeMounts {
+			if vm.Name == volumeName && vm.MountPath == mountPath {
+				added = true
+			}
+		}
+		if !added {
+			c.VolumeMounts = append(c.VolumeMounts, v1.VolumeMount{Name: volumeName, MountPath: mountPath})
+		}
+		ctrs = append(ctrs, c)
+	}
+	d.Spec.Template.Spec.Containers = ctrs
+}
--- pkg/util/manifests_test.go
+++ pkg/util/manifests_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	appsv1 "k8s.io/api/apps/v1"
 	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"testing"
 )
 
@@ -78,6 +79,23 @@ func TestAddEnvVarToDeployment(t *testing.T) {
 				assert.Equal(t, "newValue", ctrs[1].Env[0].Value)
 			},
 		},
+		{
+			name:   "idempotent call",
+			dep:    newDeployment(),
+			append: false,
+			e:      v1.EnvVar{Name: "NEW_VAR", Value: "newValue"},
+			filter: func(c v1.Container) bool { return c.Name == "first-container" },
+			assert: func(d *appsv1.Deployment, t *testing.T) {
+				AddEnvVarToDeployment(d, v1.EnvVar{Name: "NEW_VAR", Value: "newValue"}, false, func(c v1.Container) bool { return c.Name == "first-container" })
+				ctrs := d.Spec.Template.Spec.Containers
+				assert.Equal(t, 2, len(ctrs[0].Env))
+				assert.Equal(t, "ENV_VAR_EXISTING", ctrs[0].Env[0].Name)
+				assert.Equal(t, "existingValue", ctrs[0].Env[0].Value)
+				assert.Equal(t, "NEW_VAR", ctrs[0].Env[1].Name)
+				assert.Equal(t, "newValue", ctrs[0].Env[1].Value)
+				assert.Equal(t, 0, len(ctrs[1].Env))
+			},
+		},
 	}
 
 	for _, c := range cases {
@@ -86,22 +104,126 @@ func TestAddEnvVarToDeployment(t *testing.T) {
 	}
 }
 
+func TestAddVolumeMountAsSecret(t *testing.T) {
+	cases := []struct {
+		name   string
+		d      *appsv1.Deployment
+		s      *v1.Secret
+		filter func(c v1.Container) bool
+		assert func(d *appsv1.Deployment, t *testing.T)
+	}{
+		{
+			name:   "add non existing volume",
+			d:      newDeployment(),
+			s:      newSecret(),
+			filter: func(c v1.Container) bool { return true },
+			assert: func(d *appsv1.Deployment, t *testing.T) {
+				volumes := d.Spec.Template.Spec.Volumes
+				assert.Equal(t, 2, len(volumes))
+				assert.Equal(t, "test-secret", volumes[1].Name)
+				assert.Equal(t, "test-secret", volumes[1].Secret.SecretName)
+				ctrs := d.Spec.Template.Spec.Containers
+				assert.Equal(t, 2, len(ctrs[0].VolumeMounts))
+				assert.Equal(t, "test-secret", ctrs[0].VolumeMounts[1].Name)
+				assert.Equal(t, "/opt/extra", ctrs[0].VolumeMounts[1].MountPath)
+				assert.Equal(t, 1, len(ctrs[1].VolumeMounts))
+				assert.Equal(t, "test-secret", ctrs[1].VolumeMounts[0].Name)
+				assert.Equal(t, "/opt/extra", ctrs[1].VolumeMounts[0].MountPath)
+			},
+		},
+		{
+			name:   "add volume to only one container",
+			d:      newDeployment(),
+			s:      newSecret(),
+			filter: func(c v1.Container) bool { return c.Name == "first-container" },
+			assert: func(d *appsv1.Deployment, t *testing.T) {
+				volumes := d.Spec.Template.Spec.Volumes
+				assert.Equal(t, 2, len(volumes))
+				assert.Equal(t, "test-secret", volumes[1].Name)
+				assert.Equal(t, "test-secret", volumes[1].Secret.SecretName)
+				ctrs := d.Spec.Template.Spec.Containers
+				assert.Equal(t, 2, len(ctrs[0].VolumeMounts))
+				assert.Equal(t, "test-secret", ctrs[0].VolumeMounts[1].Name)
+				assert.Equal(t, "/opt/extra", ctrs[0].VolumeMounts[1].MountPath)
+				assert.Equal(t, 0, len(ctrs[1].VolumeMounts))
+			},
+		},
+		{
+			name:   "volume already added",
+			d:      newDeploymentWithMountPath("/opt/extra"),
+			s:      newSecretWithName("first-volume"),
+			filter: func(c v1.Container) bool { return c.Name == "first-container" },
+			assert: func(d *appsv1.Deployment, t *testing.T) {
+				volumes := d.Spec.Template.Spec.Volumes
+				assert.Equal(t, 1, len(volumes))
+				assert.Equal(t, "first-volume", volumes[0].Name)
+				ctrs := d.Spec.Template.Spec.Containers
+				assert.Equal(t, 1, len(ctrs[0].VolumeMounts))
+				assert.Equal(t, "first-volume", ctrs[0].VolumeMounts[0].Name)
+				assert.Equal(t, "/opt/extra", ctrs[0].VolumeMounts[0].MountPath)
+				assert.Equal(t, 0, len(ctrs[1].VolumeMounts))
+			},
+		},
+		{
+			name:   "idempotent call",
+			d:      newDeployment(),
+			s:      newSecret(),
+			filter: func(c v1.Container) bool { return c.Name == "first-container" },
+			assert: func(d *appsv1.Deployment, t *testing.T) {
+				AddVolumeMountAsSecret(d, newSecret(), "/opt/extra", func(c v1.Container) bool { return c.Name == "first-container" })
+				volumes := d.Spec.Template.Spec.Volumes
+				assert.Equal(t, 2, len(volumes))
+				assert.Equal(t, "test-secret", volumes[1].Name)
+				assert.Equal(t, "test-secret", volumes[1].Secret.SecretName)
+				ctrs := d.Spec.Template.Spec.Containers
+				assert.Equal(t, 2, len(ctrs[0].VolumeMounts))
+				assert.Equal(t, "test-secret", ctrs[0].VolumeMounts[1].Name)
+				assert.Equal(t, "/opt/extra", ctrs[0].VolumeMounts[1].MountPath)
+				assert.Equal(t, 0, len(ctrs[1].VolumeMounts))
+			},
+		},
+	}
+
+	for _, c := range cases {
+		AddVolumeMountAsSecret(c.d, c.s, "/opt/extra", c.filter)
+		c.assert(c.d, t)
+	}
+}
+
 func newDeployment() *appsv1.Deployment {
+	return newDeploymentWithMountPath("/opt/config")
+}
+
+func newDeploymentWithMountPath(mountPath string) *appsv1.Deployment {
 	return &appsv1.Deployment{
 		Spec: appsv1.DeploymentSpec{
 			Template: v1.PodTemplateSpec{
 				Spec: v1.PodSpec{
 					Containers: []v1.Container{
 						{
-							Name: "first-container",
-							Env:  []v1.EnvVar{{Name: "ENV_VAR_EXISTING", Value: "existingValue"}},
+							Name:         "first-container",
+							Env:          []v1.EnvVar{{Name: "ENV_VAR_EXISTING", Value: "existingValue"}},
+							VolumeMounts: []v1.VolumeMount{{Name: "first-volume", MountPath: mountPath}},
 						},
 						{
 							Name: "second-container",
 						},
 					},
-				},
+					Volumes: []v1.Volume{{
+						Name:         "first-volume",
+						VolumeSource: v1.VolumeSource{Secret: &v1.SecretVolumeSource{SecretName: "first-volume"}},
+					}}},
 			},
 		},
+	}
+}
+
+func newSecret() *v1.Secret {
+	return newSecretWithName("test-secret")
+}
+
+func newSecretWithName(name string) *v1.Secret {
+	return &v1.Secret{
+		ObjectMeta: metav1.ObjectMeta{Name: name},
 	}
 }
