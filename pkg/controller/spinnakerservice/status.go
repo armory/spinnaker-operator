3ff5d8ba7eaa95655aab6994f00abd50881f43a8
Cristhian Castaneda <ccastanedarivera@gmail.com>
2020-08-27 12:04:28 -0500

    feat(health-check): watch replica set

--- pkg/controller/spinnakerservice/status.go
+++ pkg/controller/spinnakerservice/status.go
@@ -10,6 +10,7 @@ import (
 	"k8s.io/client-go/tools/record"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"strings"
+	"time"
 )
 
 type statusChecker struct {
@@ -20,11 +21,12 @@ type statusChecker struct {
 }
 
 const (
-	Ok          = "OK"
-	Updating    = "Updating"
-	Unavailable = "Unavailable"
-	Na          = "N/A"
-	Failure     = "Failure"
+	Ok                                    = "OK"
+	Updating                              = "Updating"
+	Unavailable                           = "Unavailable"
+	Na                                    = "N/A"
+	Failure                               = "Failure"
+	MaxChecksWaitingForSpinnakerStability = 1
 )
 
 func newStatusChecker(client client.Client, logger logr.Logger, f interfaces.TypesFactory, evtRecorder record.EventRecorder) statusChecker {
@@ -129,6 +131,43 @@ func (s *statusChecker) getPodsByDeployment(instance interfaces.SpinnakerService
 	}
 }
 
+// isContainerInFailureState validate if container is in a failure state
+func (s *statusChecker) getReplicaSetByPod(instance interfaces.SpinnakerService, pod v1.Pod) (*appsv1.ReplicaSet, error) {
+	rs := &appsv1.ReplicaSet{}
+	rsName := ""
+	for _, or := range pod.GetOwnerReferences() {
+		if or.Kind == "ReplicaSet" {
+			rsName = or.Name
+		}
+	}
+
+	key := client.ObjectKey{instance.GetNamespace(), rsName}
+
+	err := s.client.Get(context.TODO(), key, rs)
+	if err != nil {
+		return &appsv1.ReplicaSet{}, err
+	}
+
+	return rs, nil
+}
+
+// hasExceededMaxWaitingTime validate if a replicaset has exceeded max waiting time
+func (s *statusChecker) hasExceededMaxWaitingTime(instance interfaces.SpinnakerService, pod v1.Pod) (bool, error) {
+	rs, err := s.getReplicaSetByPod(instance, pod)
+	if err != nil {
+		return false, err
+	}
+
+	if rs.Status.AvailableReplicas != rs.Status.Replicas || rs.Status.ReadyReplicas != rs.Status.Replicas {
+		diff := time.Now().Sub(rs.CreationTimestamp.Time)
+		if diff.Minutes() > MaxChecksWaitingForSpinnakerStability {
+			return true, nil
+		}
+		return false, nil
+	}
+	return false, nil
+}
+
 // getStatus check spinnaker status
 func (s *statusChecker) getStatus(instance interfaces.SpinnakerService, pods []v1.Pod) (string, error) {
 	status := Ok
@@ -140,11 +179,14 @@ func (s *statusChecker) getStatus(instance interfaces.SpinnakerService, pods []v
 	for _, p := range pods {
 		switch p.Status.Phase {
 		case v1.PodRunning:
+			timeOut, err := s.hasExceededMaxWaitingTime(instance, p)
+			if err != nil {
+				return Failure, err
+			}
+			if timeOut {
+				return Failure, nil
+			}
 			for _, cs := range p.Status.ContainerStatuses {
-				if cs.RestartCount > 1 {
-					s.evtRecorder.Eventf(instance, v1.EventTypeWarning, "DeployFailed", "Pod %s has not been able to reach a healthy state is in Phase: %s. Message: %s", p.Name, p.Status.Phase, p.Status.Reason)
-					return Failure, nil
-				}
 				if cs.State.Terminated != nil {
 					s.evtRecorder.Eventf(instance, v1.EventTypeWarning, "DeployInProgress", "Pod %s is in Phase: %s. Message: %s", p.Name, p.Status.Phase, cs.State.Terminated.Reason)
 					return Updating, nil
@@ -160,6 +202,13 @@ func (s *statusChecker) getStatus(instance interfaces.SpinnakerService, pods []v
 			}
 			break
 		case v1.PodPending:
+			timeOut, err := s.hasExceededMaxWaitingTime(instance, p)
+			if err != nil {
+				return Failure, err
+			}
+			if timeOut {
+				return Failure, nil
+			}
 			for _, cs := range p.Status.ContainerStatuses {
 				if cs.State.Waiting != nil {
 					if "ContainerCreating" == cs.State.Waiting.Reason {
