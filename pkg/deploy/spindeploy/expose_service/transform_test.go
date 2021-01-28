062a42b2e304274985e8d898664a68a8af5edf1c
Cristhian Castaneda <ccastanedarivera@gmail.com>
2021-01-27 20:16:22 -0600

    fix(expose): add unit test.

--- pkg/deploy/spindeploy/expose_service/transform_test.go
+++ pkg/deploy/spindeploy/expose_service/transform_test.go
@@ -282,3 +282,17 @@ func TestTransformHalconfig_ExposedPortRemovedFromConfig(t *testing.T) {
 	assert.Equal(t, "http://abc.com", actualOverrideUrl)
 	assert.Equal(t, "http://abc.com", (tr.(*exposeTransformer)).svc.GetStatus().APIUrl)
 }
+
+// Input: existing services running on custom port, then spin config override port
+func TestTransformHalconfig_ExposedPortOverridedFromConfig(t *testing.T) {
+	gateSvc := &corev1.Service{}
+	test.ReadYamlFile("testdata/output_service_lb.yml", gateSvc, t)
+	tr, spinSvc := transformertest.SetupTransformerFromSpinFile(&TransformerGenerator{}, "testdata/spinsvc_override_port.yml", t, gateSvc)
+	gen := &generated.SpinnakerGeneratedConfig{}
+	test.AddServiceToGenConfig(gen, "gate", "testdata/input_service.yml", t)
+	spinSvc.GetExposeConfig().Service.PublicPort = 0
+
+	err := tr.TransformManifests(context.TODO(), gen)
+	assert.Nil(t, err)
+	assert.Equal(t, int32(8089), gen.Config["gate"].Service.Spec.Ports[0].Port)
+}
